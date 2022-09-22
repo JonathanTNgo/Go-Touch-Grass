2cf7ef01c4d66e6ab424d5b0bb371152cf2c1313
gamedevalan <gamedevalan01@gmail.com>
2022-09-22 17:47:27 -0500

    ConfettiScreen now links to home screen

--- app/src/main/java/com/john/gotouchgrass/ConfettiScreen.kt
+++ app/src/main/java/com/john/gotouchgrass/ConfettiScreen.kt
@@ -1,14 +1,27 @@
 package com.john.gotouchgrass
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ConfettiScreen : AppCompatActivity() {
+    private var homeButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confetti_screen)
-
+        homeButton = findViewById(R.id.button)
         // TODO: ON BUTTON CLICK, FINISH ACTIVITY AND RETURN HOME
         // (KILL EVERY ACTIVITY, START A FRESH NEW ACTIVITY)
+        homeButton!!.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
+
 }
