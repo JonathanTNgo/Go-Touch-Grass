7320a95317f0dec02bfc9a52b6ff4a8fe1754ab6
Cameron Courtney <cameroncourtney@Camerons-Air.attlocal.net>
2022-09-23 19:28:52 -0500

    Added Confetti

--- app/src/main/java/com/john/gotouchgrass/ConfettiScreen.kt
+++ app/src/main/java/com/john/gotouchgrass/ConfettiScreen.kt
@@ -5,9 +5,37 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import nl.dionsegijn.konfetti.core.Angle
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.Spread
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import nl.dionsegijn.konfetti.xml.KonfettiView
+import java.util.concurrent.TimeUnit
 
+// Class for creating the confetti animation
+// ** CREDIT TO https://github.com/DanielMartinus/Konfetti/ **
+class Presets {
+    companion object {
+        fun rain(): List<Party> {
+            return listOf(
+                Party(
+                    speed = 0f,
+                    maxSpeed = 15f,
+                    damping = 0.9f,
+                    angle = Angle.BOTTOM,
+                    spread = Spread.ROUND,
+                    colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                    emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(100),
+                    position = Position.Relative(0.0, 0.0).between(Position.Relative(1.0, 0.0))
+                )
+            )
+        }
+    }
+}
 
 class ConfettiScreen : AppCompatActivity() {
+    private lateinit var viewKonfetti: KonfettiView
     private var homeButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +46,10 @@ class ConfettiScreen : AppCompatActivity() {
         // The text element is set to display the amount of time a user spent outside.
         var textElement: TextView = findViewById(R.id.time_text)
         textElement.text = this.resources?.getString(R.string.minute_string, RatingScreen.timeSpent)
+
+        // Confetti set up
+        viewKonfetti = findViewById(R.id.konfettiView)
+        viewKonfetti.start(Presets.rain())
 
         // When the home button is clicked, a new MainActivity is started with the default
         // image of grass.
@@ -30,5 +62,4 @@ class ConfettiScreen : AppCompatActivity() {
         // There is no back button available for this page.
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
-
 }
