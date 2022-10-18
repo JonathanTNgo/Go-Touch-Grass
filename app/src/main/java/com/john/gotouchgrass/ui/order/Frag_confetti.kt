aa511f9753e08d5dc6bee7c2366d7a74bd34946f
gamedevalan <gamedevalan01@gmail.com>
2022-10-18 16:39:02 -0500

    ConfettiScreen now rains.

--- app/src/main/java/com/john/gotouchgrass/ui/order/Frag_confetti.kt
+++ app/src/main/java/com/john/gotouchgrass/ui/order/Frag_confetti.kt
@@ -12,8 +12,37 @@ import androidx.navigation.fragment.findNavController
 import com.john.gotouchgrass.R
 import com.john.gotouchgrass.databinding.FragmentFragConfettiBinding
 import com.john.gotouchgrass.model.GrassViewModel
+import nl.dionsegijn.konfetti.core.Angle
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.Spread
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import nl.dionsegijn.konfetti.xml.KonfettiView
+import java.util.concurrent.TimeUnit
+
+// Class for creating the confetti animation
+// ** CREDIT TO https://github.com/DanielMartinus/Konfetti/ **
+class Presets {
+    companion object {
+        fun rain(): List<Party> {
+            return listOf(
+                Party(
+                    speed = 0f,
+                    maxSpeed = 15f,
+                    damping = 0.9f,
+                    angle = Angle.BOTTOM,
+                    spread = Spread.ROUND,
+                    colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                    emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(100),
+                    position = Position.Relative(0.0, 0.0).between(Position.Relative(1.0, 0.0))
+                )
+            )
+        }
+    }
+}
 
 class Frag_confetti : Fragment() {
+    private lateinit var viewKonfetti: KonfettiView
     private var _binding: FragmentFragConfettiBinding? = null
     private val binding get() = _binding!!
 
@@ -25,7 +54,10 @@ class Frag_confetti : Fragment() {
     ): View? {
         _binding = FragmentFragConfettiBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        binding.timeText.text = this.resources?.getString(R.string.minute_string, viewModel.getTime());
+        binding.timeText.text = this.resources.getString(R.string.minute_string, viewModel.getTime())
+        // Confetti set up
+        viewKonfetti = binding.konfettiView
+        viewKonfetti.start(Presets.rain())
         binding.homeButton.setOnClickListener {
             findNavController().navigate(R.id.action_frag_confetti_to_grassScreen)
         }
