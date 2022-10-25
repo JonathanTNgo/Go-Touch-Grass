0408c8b6c783d878362523ab74e56741e020b17e
jonathanTngo <jonathantngo@utexas.edu>
2022-10-25 16:51:59 -0600

    Removed getMinutes and changed structure of time. Viewmodel now holds the time in seconds. Added new function in viewModel that will return the string for the time spent outside (changes with time size)

--- app/src/main/java/com/john/gotouchgrass/model/GrassViewModel.kt
+++ app/src/main/java/com/john/gotouchgrass/model/GrassViewModel.kt
@@ -6,24 +6,43 @@ import java.lang.System.currentTimeMillis
 class GrassViewModel: ViewModel() {
     private var timeStart = 0
     private var timeTotal = 0
+    private var timeSeconds = 0
 
-    // text
-    // private var text
-
+    // Starts timer
     fun startTime() {
         timeStart = currentTimeMillis().toInt();
     }
 
+    // Stops timer and stores the time length in seconds (initially in milliseconds)
     fun stopTime() {
         timeTotal = currentTimeMillis().toInt() - timeStart
+//        timeSeconds = (timeTotal / 1000 % 60)
+        timeSeconds = 3680
     }
 
-    fun getMinutes(): Double {
-        return (timeTotal / 1000 / 60).toDouble()
-    }
-
+    // Returns the timeSeconds as a double for printf
     fun getSeconds(): Double {
-        return (timeTotal / 1000 % 60).toDouble()
+        return timeSeconds.toDouble()
+    }
+
+    // Returns a string representing the time.
+    // If time < 60 seconds, string will say seconds
+    // If time > 60 seconds, string will say minutes + seconds
+    // If time > 60 minutes, string will say hours + minutes
+    fun getTime(): String {
+        when (timeSeconds)
+        {
+            // Print in seconds
+            in 0..59 -> return "You spent " + timeSeconds + " seconds outside!"
+            // Print only for 1 minute
+            60 -> return "You spent " + (timeSeconds / 60) + " minute outside!"
+            // Print in minutes and seconds
+            in 61..3599 -> return "You spent " + (timeSeconds / 60) + " minutes and " + (timeSeconds % 60) + " second(s) outside!"
+            // Print only for 1 hour
+            3600 -> return "You spent " + (timeSeconds / 60 / 60) + " hour outside!"
+            // Print in hours and minutes
+            else -> return "You spent " + (timeSeconds / 60 / 60) + " hours and " + (timeSeconds / 60 % 60) + " minute(s) outside!"
+        }
     }
 
     fun resetTime() {
