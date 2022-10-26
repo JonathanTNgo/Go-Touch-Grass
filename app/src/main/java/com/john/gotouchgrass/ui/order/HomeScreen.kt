e8ab92b4c38d9472a9c2e9dd048c4ca0f2a4d440
gamedevalan <gamedevalan01@gmail.com>
2022-10-26 15:02:11 -0500

    Some camera code added.

--- app/src/main/java/com/john/gotouchgrass/ui/order/HomeScreen.kt
+++ app/src/main/java/com/john/gotouchgrass/ui/order/HomeScreen.kt
@@ -1,10 +1,20 @@
 package com.john.gotouchgrass.ui.order
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.john.gotouchgrass.R
@@ -16,6 +26,8 @@ class HomeScreen : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: GrassViewModel by activityViewModels()
+    val REQUEST_CODE = 200
+    lateinit var logo: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,12 +39,95 @@ class HomeScreen : Fragment() {
             viewModel.stopTime()
             findNavController().navigate(R.id.action_homeScreen_to_frag_rating)
         }
+        super.onCreate(savedInstanceState)
+        root.setOnTouchListener(object : OnSwipeTouchListener(activity) {
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                Log.d( "Left", "WENT left")
+                Toast.makeText(activity, "Swipe Left gesture detected",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+            override fun onSwipeRight() {
+                super.onSwipeRight()
+                Log.d( "Right", "WENT right")
+                Toast.makeText(
+                    activity,
+                    "Swipe Right gesture detected",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onSwipeUp() {
+                super.onSwipeUp()
+                Toast.makeText(activity, "Swipe up gesture detected", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            override fun onSwipeDown() {
+                super.onSwipeDown()
+                Toast.makeText(activity, "Swipe down gesture detected", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+
+        binding.cameraButton.setOnClickListener {
+            if (activity?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                } !==
+                PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                    requestPermissions(arrayOf(Manifest.permission.CAMERA), 1)
+                } else {
+                    requestPermissions(arrayOf(Manifest.permission.CAMERA), 1)
+                }
+            }
+        }
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((activity?.let {
+                            ContextCompat.checkSelfPermission(
+                                it,
+                                Manifest.permission.CAMERA)
+                        } ===
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(activity, "Permission Granted", Toast.LENGTH_SHORT).show()
+                        capturePhoto()
+                    }
+                } else {
+                    Toast.makeText(activity, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    fun capturePhoto() {
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+            logo = binding.iv
+            logo.setImageBitmap(data.extras?.get("data") as Bitmap)
+        }
     }
 }
 
