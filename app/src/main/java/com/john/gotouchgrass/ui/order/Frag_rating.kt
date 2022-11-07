9c50da694b2a4eaa85259c0cf74203e5ecfc8964
Cameron Courtney <cameroncourtney@Camerons-MacBook-Air.local>
2022-11-07 17:30:36 -0600

    Added Landscape Files

--- app/src/main/java/com/john/gotouchgrass/ui/order/Frag_rating.kt
+++ app/src/main/java/com/john/gotouchgrass/ui/order/Frag_rating.kt
@@ -29,25 +29,25 @@ class Frag_rating : Fragment() {
         val root: View = binding.root
 
         // Disables the hint when the activity text box is clicked
-        binding.activityText.setOnFocusChangeListener(View.OnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) binding.activityText.setHint("")
-            else binding.activityText.setHint("What did you do?\\n Edit me!")
+        binding.activityText?.setOnFocusChangeListener(View.OnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) binding.activityText?.setHint("")
+            else binding.activityText?.setHint("What did you do?\\n Edit me!")
         })
 
         // When button is clicked, save text and switch to confetti fragment
-        binding.submit.setOnClickListener {
+        binding.submit?.setOnClickListener {
             // Sample code for adding image onto recycler view
-//            val moment: GrassMoment? = viewModel.getImage()?.let { it1 ->
-//                GrassMoment(viewModel.getTime(), binding.activityText.editText?.text.toString(),
-//                    it1
-//                )
-//            }
-//
-//            if (moment != null) {
-//                DataSource.grassMoments.add(moment)
-//            }
+    //            val moment: GrassMoment? = viewModel.getImage()?.let { it1 ->
+    //                GrassMoment(viewModel.getTime(), binding.activityText.editText?.text.toString(),
+    //                    it1
+    //                )
+    //            }
+    //
+    //            if (moment != null) {
+    //                DataSource.grassMoments.add(moment)
+    //            }
 
-            val moment: GrassMoment = GrassMoment(viewModel.getTime(), binding.activityText.editText?.text.toString(), viewModel.getImage())
+            val moment: GrassMoment = GrassMoment(viewModel.getTime(), binding.activityText?.editText?.text.toString(), viewModel.getImage())
             DataSource.grassMoments.add(moment)
             findNavController().navigate(R.id.action_frag_rating_to_frag_confetti)
         }
